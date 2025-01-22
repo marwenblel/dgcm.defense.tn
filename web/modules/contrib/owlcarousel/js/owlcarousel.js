b2c8508c5f9d7e919ee7e9fa791d72530b41ea48
marwenblel <marwen.blel@gmail.com>
2025-01-22 12:19:33 +0100

    changes.

--- web/modules/contrib/owlcarousel/js/owlcarousel.js
+++ web/modules/contrib/owlcarousel/js/owlcarousel.js
@@ -8,6 +8,9 @@
       $('.owl-slider-wrapper', context).each(function () {
         var $this = $(this);
         var $this_settings = $.parseJSON($this.attr('data-settings'));
+		/**Add RTL TRUE to SETTINGS**START**/
+		$this_settings.rtl = true;
+		/**Add RTL TRUE to SETTINGS**END**/
         $this.owlCarousel($this_settings);
       });
     }
